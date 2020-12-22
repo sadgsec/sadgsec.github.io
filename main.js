3295d737894c108f44e8e1b36b6b4fedd4fdca28
sadgsec <38287372+sadgsec@users.noreply.github.com>
2020-12-22 00:53:07 +0000

    Initial push

---
+++ main.js
@@ -0,0 +1,5 @@
+function myFunction() {
+  let x = screen.width;
+  let y = screen.height;
+  document.getElementById("button").innerHTML = `<button style="width:${x};height:${y}">CLICK HERE TO COMMIT CRIME</button>`;
+}
